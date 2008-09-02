772d4a6a168d1c1123937bff9eeded55973f43a3
Piotr Lewandowski <piotr.lewandowski+sinntp@gmail.com>
2008-09-02 17:01:51 +0200

    Add filtering capability to plugins.

--- plugins.py
+++ plugins.py
@@ -5,9 +5,11 @@ def strip_headers(headers = 'To,Cc,Bcc', message = None):
     headers = headers.split(',')
     for header in headers:
         del message[header]
+    return message
 
 def mimify(type = 'text/plain', charset = 'US-ASCII', message = None):
     if 'Content-Type' not in message:
         message['Content-Type'] = '%(type)s; charset=%(charset)s' % locals()
+    return message
 
 # vim:ts=4 sw=4 et
