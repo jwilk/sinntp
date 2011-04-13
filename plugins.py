9da39c9e6bbf833fa56ebcd2ed2256ff98e29efe
Jakub Wilk <jwilk@jwilk.net>
2011-04-13 17:25:15 +0200

    A few PEP-8-ish corrections.

--- plugins.py
+++ plugins.py
@@ -16,13 +16,13 @@
 def debug(*args, **kwargs):
     print 'debug(*%r, **%r)' % (args, kwargs)
 
-def strip_headers(headers = 'To,Cc,Bcc', message = None):
+def strip_headers(headers='To,Cc,Bcc', message=None):
     headers = headers.split(',')
     for header in headers:
         del message[header]
     return message
 
-def mimify(type = 'text/plain', charset = 'US-ASCII', message = None):
+def mimify(type='text/plain', charset='US-ASCII', message=None):
     if 'Content-Type' not in message:
         message['Content-Type'] = '%(type)s; charset=%(charset)s' % locals()
     return message
