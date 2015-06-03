ab0098b6aae70d1ca5b0051604754c77ab02e327
Jakub Wilk <jwilk@jwilk.net>
2015-06-03 18:16:55 +0200

    Update copyright information.

--- plugins.py
+++ plugins.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2008, 2009, 2010, 2011
+# Copyright © 2008-2015
 #   Piotr Lewandowski <piotr.lewandowski@gmail.com>,
 #   Jakub Wilk <jwilk@jwilk.net>.
 #
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2011, 2012
+# Copyright © 2011-2015
 #   Jakub Wilk <jwilk@jwilk.net>.
 #
 # This program is free software; you can redistribute it and/or modify it
