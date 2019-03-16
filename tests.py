60c0bbfbd5942f9a55c22b46b32d738546b8c797
Jakub Wilk <jwilk@jwilk.net>
2019-03-16 19:06:26 +0100

    Silence Pylint's pointless-statement.

--- tests.py
+++ tests.py
@@ -12,7 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
-...  # Python 3 is required
+type(...)  # Python 3 is required
 
 import os
 import unittest
